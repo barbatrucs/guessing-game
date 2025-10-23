d9e24f3e19e1d652c53c6c0acd434ca1f0ce7cb5
Barbatruc <6886536+barbatrucs@users.noreply.github.com>
2025-10-23 16:13:46 +0200

    Comparing the Guess to the Secret Number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::io;
 
 use rand::Rng;
@@ -11,11 +12,20 @@ fn main() {
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess= String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // Shadowing the previous value of guess
+    let guess: u32 = guess.trim().parse().expect("Please type a number.");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!")
+    }
 }
